7c590c61a81bb91df6849f0128c79e7193f1cf1c
omkar <omkardudhewar121@gmail.com>
2022-12-22 14:34:05 +0530

    added: sample email tests

--- UserRegistration.java
+++ UserRegistration.java
@@ -15,7 +15,7 @@ public class UserRegistration {
         /*
         first letter is Capital and other small letters =>minimum 3
          */
-        String regex = "[A-Z]+[a-z]{3}$";
+        String regex = "^[A-Z]+[a-z]{2}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstName);
         boolean result = matcher.matches();
@@ -26,14 +26,15 @@ public class UserRegistration {
             validFirstName();
         }
     }
-    static void validLastName(){
+
+    static void validLastName() {
         /*
         UC2->Last name starts with Cap and has minimum 3 character
          */
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Last Name");
         String lastName = scanner.nextLine();
-        String regex = "[A-Z]+[a-z]{3}$";
+        String regex = "^[A-Z]+[a-z]{2}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lastName);
         boolean result = matcher.matches();
@@ -41,22 +42,22 @@ public class UserRegistration {
             System.out.println(lastName + "\nLast Name is Valid");
         } else {
             System.out.println(lastName + " Invalid Name");
-            validFirstName();
             validLastName();
         }
     }
-    static void validEmail(){
+
+    static void validEmail() {
         /*
         UC3-> Valid Email E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co)
               and 2 optional (xyz & in) with precise @ and . positions.
-         */
+        */
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter E-mail");
         String email = scanner.nextLine();
         /*
         pattern of e-mail string => abc.xyz@bl.co.in
          */
-        String regex = "[a-z]{3}.[a-z]{3}@[a-z]{2}.[a-z]{2}.[a-z]{2}$";
+        String regex = "^[a-z]{3}.[a-z]{3}@[a-z]{2}.[a-z]{2}.[a-z]{2}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         boolean result = matcher.matches();
@@ -64,12 +65,11 @@ public class UserRegistration {
             System.out.println(email + "\nE-mail is Valid");
         } else {
             System.out.println(email + " Invalid E-mail ");
-            validFirstName();
-            validLastName();
             validEmail();
         }
     }
-    static void validContactNo(){
+
+    static void validContactNo() {
         /*
         UC4-> valid Mobile Format - E.g. 91 9919819801 - Country
               code follow by space and 10-digit number
@@ -77,7 +77,7 @@ public class UserRegistration {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Contact No-");
         String contactNo = scanner.nextLine();
-        String regex = "[0-9]{2} [0-9]{10}$";
+        String regex = "^[0-9]{2} [0-9]{10}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(contactNo);
         boolean result = matcher.matches();
@@ -85,13 +85,11 @@ public class UserRegistration {
             System.out.println(contactNo + "\nContact No.is Valid");
         } else {
             System.out.println(contactNo + "\nInvalid Contact No.");
-            validFirstName();
-            validLastName();
-            validEmail();
             validContactNo();
         }
     }
-    static void validPasswordRule(){
+
+    static void validPasswordRule() {
         /*
         UC5-> password Rule1– minimum 8 Characters
         UC6-> at least 1 upper case
@@ -101,22 +99,35 @@ public class UserRegistration {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Password");
         String password1 = scanner.nextLine();
-        String regex = "[A-Z]+[0-9]+[@#%&*!$^]{1}+[a-z]{5}$";
+        String regex = "^[A-Z]+[0-9]+[@#%&*!$^]{1}+[a-z]{5}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password1);
         boolean result = matcher.matches();
         if (result) {
-            System.out.println(password1+"\nPassword is Valid");
+            System.out.println(password1 + "\nPassword is Valid");
             System.out.println("All Rules are Passed");
         } else {
             System.out.println(password1 + "\nInvalid Password");
-            validFirstName();
-            validLastName();
-            validEmail();
-            validContactNo();
             validPasswordRule();
         }
     }
+
+    public static void checkEmailSamples() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter E-m@il i'd");
+        String emailSamples = scanner.nextLine();
+        String regex = "^[a-z]{3,}[+-.]?[a-z]{3}[0-9]{3}[@]{1}[a-z]+[.]?[a-z]{3}[.]?[a-z]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailSamples);
+        boolean result = matcher.matches();
+        if (result) {
+            System.out.println(emailSamples + "\nE-m@il i'd is Valid");
+        } else {
+            System.out.println(emailSamples + "\nInvalid E-m@il i'd");
+            checkEmailSamples();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Program");
         validFirstName();
@@ -124,5 +135,6 @@ public class UserRegistration {
         validEmail();
         validContactNo();
         validPasswordRule();
+        checkEmailSamples();
     }
 }
